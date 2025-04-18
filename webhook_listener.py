5dfbd2713312be151dfc70942eda11240da6a662
TheSachinOp <pricexrajput@gmail.com>
2025-04-19 01:39:37 +0530

    .

--- webhook_listener.py
+++ webhook_listener.py
@@ -1,12 +1,13 @@
+# webhook_listener.py
+
 from flask import Flask, request
 import subprocess
-import os
 
 app = Flask(__name__)
 
 @app.route("/github-webhook", methods=["POST"])
 def webhook():
-    subprocess.run(["git", "-C", "/path/to/your/bot", "pull"])
+    subprocess.run(["git", "-C", "/home/TheSachinOp/mini", "pull"])
     return "OK", 200
 
 if __name__ == "__main__":
